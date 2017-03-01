8edaf44cb3123dc4b05f2228f40bafbaebc46819
Adrien <adrien@arccoza.com>
2017-03-01 16:55:21 +0900

    Changed view route to handle comments directly. Comment route deprecated.

--- __init__.py
+++ __init__.py
@@ -33,6 +33,9 @@ def is_form_cancelled(redir):
 @BlogPost.q('ORDER BY created DESC LIMIT 10 OFFSET {{offset}}', 'posts')
 @User.is_available()
 def root(offset=0, posts=None, user=None):
+  '''
+  View a list of posts.
+  '''
   user = user or None
   posts = posts or None
   next = offset + 10
@@ -41,25 +44,42 @@ def root(offset=0, posts=None, user=None):
                             posts=posts, next=next, prev=prev)
 
 
-@app.route("/view/<string:post_id>")
+@app.route("/view/<string:post_id>", methods=['GET', 'POST'])
+@is_form_cancelled(lambda: redirect(request.path))
 @BlogPost.q('WHERE uid = \'{{post_id}}\'', 'post')
 @BlogComment.q('WHERE ANCESTOR IS {{post.get().key|safe}}', 'comments')
 @User.is_available()
 def view(post_id=None, post=None, comments=None, user=None):
+  '''
+  View and comment on a post.
+  '''
   user = user or None
   post = post.get() if post else None
   comments = comments or None
-  if post:
+  if user and post and request.method == 'POST':
+    comment = BlogComment(parent=post.key)
+    comment.fill(content=request.form.get('content', None), author=user.key)
+    if not comment.empty('content'):
+      comment.put()
+      return redirect(request.path + '#comments')
+    else:
+      return render_template('view.html', page=None, user=user,
+                          post=post, comments=comments,
+                          comment=comment)
+  elif post:
     return render_template('view.html', page=None, user=user,
                             post=post, comments=comments)
   else:
-    abort(404)
+    return abort(404)
 
 
 @app.route("/fave/<string:post_id>", methods=['POST'])
 @User.is_active(lambda: abort(403))
 @BlogPost.q('WHERE uid = \'{{post_id}}\'', 'post')
 def fave(post_id=None, post=None, user=None):
+  '''
+  Post to this route with a user and post it, and fave it.
+  '''
   user = user or None
   post = post.get() if post else None
   res = None
@@ -78,20 +98,23 @@ def fave(post_id=None, post=None, user=None):
     return abort(404)
 
 
-@app.route("/comment/<string:post_id>", methods=['POST'])
-@User.is_active(lambda: abort(403))
-@BlogPost.q('WHERE uid = \'{{post_id}}\'', 'post')
-def comment(post_id=None, post=None, user=None):
-  user = user or None
-  post = post.get() if post else None
-  if post:
-    comment = BlogComment(parent=post.key)
-    comment.fill(content=request.form.get('content'), author=user.key)
-    comment.put()
-    print(comment)
-    return redirect(request.referrer + '#comments')
-  else:
-    return abort(404)
+# @app.route("/comment/<string:post_id>", methods=['POST'])
+# @User.is_active(lambda: abort(403))
+# @is_form_cancelled(lambda: redirect(request.referrer))
+# @BlogPost.q('WHERE uid = \'{{post_id}}\'', 'post')
+# def comment(post_id=None, post=None, user=None):
+#   '''
+#   Post to this route to add comments. INTEGRATED INTO view route.
+#   '''
+#   user = user or None
+#   post = post.get() if post else None
+#   if post:
+#     comment = BlogComment(parent=post.key)
+#     comment.fill(content=request.form.get('content'), author=user.key)
+#     comment.put()
+#     return redirect(request.referrer + '#comments')
+#   else:
+#     return abort(404)
 
 
 @app.route("/edit/", defaults={'post_id': ''}, methods=['GET', 'POST'])
@@ -101,6 +124,9 @@ def comment(post_id=None, post=None, user=None):
 @BlogPost.q('WHERE ANCESTOR IS {{user.key|safe}} AND uid = \'{{post_id}}\'', 'post')
 @User.is_owner('post', lambda: abort(403))
 def edit(post_id=None, post=None, user=None):
+  '''
+  Create or edit a post.
+  '''
   user = user or None
   print('-------------------------------------------')
   print('post query:', post)
@@ -134,6 +160,9 @@ def edit(post_id=None, post=None, user=None):
 @BlogPost.q('WHERE uid = \'{{post_id}}\'', 'post')
 @User.is_owner('post', lambda: abort(403))
 def delete(post_id=None, post=None, user=None):
+  '''
+  Delete a post.
+  '''
   post = post.get() if post else None
   if request.method == 'POST':
     if post:
@@ -149,8 +178,12 @@ def delete(post_id=None, post=None, user=None):
 @is_form_cancelled(lambda: redirect(url_for('root')))
 @User.is_inactive(lambda: redirect(url_for('root')))
 def signup():
+  '''
+  Register a new user.
+  '''
   user = None
   pass_verified = None
+  next = request.args.get('next', None)
   if request.method == 'POST':
     try:
       user = User()
@@ -159,13 +192,14 @@ def signup():
       if user.valid() and user.unique and pass_verified:
         user.put()
         sec.token = {'usr': user.username, 'uid': user.uid}
-        return redirect(url_for('root'))
+        return redirect(next or url_for('root'))
     except Exception as ex:
       print('bad user')
       print(ex)
   return render_template('signup.html',
                           page=None, user=user,
-                          form_action='/signup/', pass_verified=pass_verified)
+                          form_action=url_for('signup', next=next),
+                          pass_verified=pass_verified)
 
 
 @app.route("/signin/", methods=['GET', 'POST'])
@@ -173,9 +207,13 @@ def signup():
 @is_form_cancelled(lambda: redirect(url_for('root')))
 @User.is_inactive(lambda: redirect(url_for('root')))
 def signin():
+  '''
+  Verify and signin a user.
+  '''
   user = None
   user_verified = False
   pass_verified = False
+  next = request.args.get('next', None)
   if request.method == 'POST':
     try:
       user = User.query(User.username == request.form.get('username', '')).get()
@@ -186,13 +224,14 @@ def signin():
       User.password.verify(request.form.get('password'), user.password)):
       sec.token = {'usr': user.username, 'uid': user.uid}
       pass_verified = True
-      return redirect(url_for('root'))
+      return redirect(next or url_for('root'))
     else:
       user = User()
       user.fill(username=request.form.get('username'))
   return render_template('signin.html',
                           page=None, user=user,
-                          form_action='/signin/', user_verified=user_verified,
+                          form_action=url_for('signin', next=next),
+                          user_verified=user_verified,
                           pass_verified=pass_verified)
 
 
@@ -201,11 +240,16 @@ def signin():
 @is_form_cancelled(lambda: redirect(url_for('root')))
 @User.is_active(lambda: redirect(url_for('signin')))
 def signout(user=None):
+  '''
+  Signout the current user.
+  '''
+  next = request.args.get('next', None)
   if request.method == 'POST':
     sec.token = {}
-    return redirect(url_for('root'))
+    return redirect(next or url_for('root'))
   return render_template('signout.html',
-                          page=None, user=user, form_action='/signout/')
+                          page=None, user=user,
+                          form_action=url_for('signout', next=next))
 
 
 if __name__ == "__main__":
